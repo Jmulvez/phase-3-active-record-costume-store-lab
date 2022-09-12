e4772a5414bbb77a7e14872ea5cbc60c2006f724
John <johnmulvey48@yahoo.com>
2022-09-12 14:47:08 -0400

    git@github.com:Jmulvez/phase-3-active-record-costume-store-lab.git

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,10 @@
-# Create your Costume class here
-# It should inherit from ActiveRecord::Base
+class CostumeStore < ActiveRecord::Migration[6.1]
+    def change
+        create_table :candies do |t|
+            t.string :name
+            t.float :price
+            t.string :size
+            t.string :image_url
+          end
+    end
+  end
